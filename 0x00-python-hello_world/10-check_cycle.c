ab38711c25832ab7536228a1ce2d502bb9e83631
BrolinsZack <zackbrolins@gmail.com>
2023-10-03 07:09:07 -0700

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,29 +1,29 @@
 #include "lists.h"
 
 /**
- * check_cycle - Function that checks if a singly linked list 
-                 contains a cycle in it.
+ * check_cycle - a function in C that checks if a singly linked list 
+                 has a cycle in it.
  * @list: A singly-linked list.
  *
  * Return: 0 if no cycle else 1
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *tea, *coffee;
+	listint_t *car, *truck;
 
 	if (list == NULL || list->next == NULL)
 		return (0);
 
-	canoe = list->next;
-	yatch = list->next->next;
+	car = list->next;
+	truck = list->next->next;
 
-	while (tea && coffee && coffee->next)
+	while (car && truck && truck->next)
 	{
-		if (tea == coffee)
+		if (car == truck)
 			return (1);
 
-		tea = tea->next;
-		coffee = coffee->next->next;
+		car = car->next;
+		truck = truck>next->next;
 	}
 
 	return (0);
