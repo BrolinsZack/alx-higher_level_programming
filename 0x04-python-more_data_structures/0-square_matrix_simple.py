3c37dc072317e6f610d72da2de4c7138d584de18
BrolinsZack <zackbrolins@gmail.com>
2023-10-11 10:59:13 -0700

    0-square_matrix_simple.py test

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
-    new_list = []
-    for row in matrix:
-        new_row = sq_list(row)
-        new_list.append(new_row)
-    return new_list
-
-
-def sq_list(x):
-    return [i**2 for i in x] 
+    new_matrix = [row[:] for row in matrix]
+    for idx, row in enumerate(new_matrix):
+        for idx2, col in enumerate(new_matrix):
+            new_matrix[idx][idx2] = row[idx2] ** 2
+    return new_matrix 
